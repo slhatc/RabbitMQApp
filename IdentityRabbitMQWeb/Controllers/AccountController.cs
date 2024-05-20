451c972e6b12990721f0cc3f6df59fe735cfdcec
slhatc50 <slhatc50@outlook.com>
2024-05-20 14:08:55 +0300

    Add project files.

---
+++ IdentityRabbitMQWeb/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityRabbitMQWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email,string Password)
+        {
+            var hasUser =await _userManager.FindByEmailAsync(Email);
+            if (hasUser == null)
+            {
+                return View();
+            }
+            var signResult = await _signInManager.PasswordSignInAsync(hasUser, Password, true, false);
+            if (!signResult.Succeeded)
+            {
+                return View();
+            }
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ IdentityRabbitMQWeb/Data/SeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityRabbitMQWeb.Data
+{
+    public static class SeedData
+    {
+        public static async void AddSeedData(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                appDbContext.Database.Migrate();
+
+                if (!appDbContext.Users.Any())
+                {
+                    userManager.CreateAsync(new IdentityUser() { UserName = "deneme", Email = "deneme@gmail.com" }, "Test123*").Wait();
+                    userManager.CreateAsync(new IdentityUser() { UserName = "deneme2", Email = "deneme2@gmail.com" }, "Test123*").Wait();
+                }
+            }
+        }
+    }
+}
