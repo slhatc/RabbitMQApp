a845f110c029cb472688683c2eb95f39e697fe3b
slhatc50 <slhatc50@outlook.com>
2024-05-21 23:00:23 +0300

    WorkerService eklendi..

---
+++ WorkerService/Program.cs
@@ -0,0 +1,18 @@
+using RabbitMQ.Client;
+using WorkerService.Services;
+using WorkerService;
+using WorkerService.Models;
+using Microsoft.EntityFrameworkCore;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext,services) =>
+    {
+        IConfiguration Configuration = hostContext.Configuration;
+        services.AddDbContext<AdventureWorks2017Context>(options => { options.UseSqlServer(Configuration.GetConnectionString("SqlServer")); }); 
+        services.AddSingleton<RabbitMQClientService>();
+        services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")!) });
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
---
+++ WorkerService/Services/RabbitMQClientService.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+
+namespace WorkerService.Services
+{
+    public class RabbitMQClientService : IDisposable
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _channel;
+        private readonly ILogger<RabbitMQClientService> _logger;
+
+        public static string RoutingExcel = "excel-route-file";
+        public static string QueueName = "queue-excel";
+
+        public RabbitMQClientService(ConnectionFactory connectionFactory, ILogger<RabbitMQClientService> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+        }
+
+        public IModel Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+            if (_channel is {IsOpen:true }) 
+            {
+                return _channel;
+            }
+
+            _channel = _connection.CreateModel();
+            
+            _logger.LogInformation("RabbitMQ ile bağlantı kuruldu.");
+
+
+            return _channel;
+
+        }
+
+        public void Dispose()
+        {
+            _channel?.Close();
+            _channel?.Dispose();
+            _connection?.Close();
+            _connection?.Dispose();
+
+            _logger.LogInformation("RabbitMQ ile bağlantı koptu");
+        }
+    }
+}
